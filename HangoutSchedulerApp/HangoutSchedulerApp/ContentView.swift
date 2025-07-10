1cee9544f017b241fd4242a98eecec2eecd08c55
Jonathan Hesler <jeh8443@rit.edu>
2025-07-09 21:47:01 -0400

    Initial Commit

---
+++ HangoutSchedulerApp/HangoutSchedulerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HangoutSchedulerApp
+//
+//  Created by Jonathan Hesler on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HangoutSchedulerApp/HangoutSchedulerApp/HangoutSchedulerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutSchedulerAppApp.swift
+//  HangoutSchedulerApp
+//
+//  Created by Jonathan Hesler on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HangoutSchedulerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
