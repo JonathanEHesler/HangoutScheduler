f2d915aa5536bd53c787de41a09bde897a05f55f
Jonathan Hesler <jeh8443@rit.edu>
2025-07-10 19:30:01 -0400

    Starter Hangout struct

--- HangoutSchedulerApp/HangoutSchedulerApp/models/Hangout.swift
+++ HangoutSchedulerApp/HangoutSchedulerApp/models/Hangout.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Hangout {
+    var name: String
+    var location: String
+    var description: String = ""
+    var date: SelectedDate
+    //Defaults to 0 in case a specific date & time is not yet selected
+    var time: TimeInterval = 0
+    
+    //Allows for a hangout to be provided with a specific date or a date range
+    enum SelectedDate {
+        case setDate(Date)
+        case setDateRange(start: Date, end: Date)
+    }
+}
